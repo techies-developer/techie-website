5c57a17bb55b3b2cc558b1bef1a686038de69d45
ayinlaabel <kraneabel@gmail.com>
2024-02-19 16:25:41 +0100

    tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,15 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    colors: {
+      brand: "#B00000",
+      black: "#2E2E2E",
+      body: "#808080",
+    },
+    fontFamily: {
+      sans: ["Poppins", "sans-serif"],
+      serif: ["Merriweather", "serif"],
+    },
     extend: {},
   },
   plugins: [],
